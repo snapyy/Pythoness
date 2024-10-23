bb941f85a1f2c25fa1c5221f83141c10e7141975
snapyy <210510113165@paruluniversity.ac.in>
2024-10-23 15:43:16 +0530

    Add files via upload

--- problem_2.py
+++ problem_2.py
@@ -7,3 +7,39 @@
 
 #########2. Write a python program to find remainder when a number is divided by z.
 
+# a=34 
+# b=5
+
+# print("Remainder when a is divided by b is: ",a%b)
+
+
+#########3. Check the type of variable assigned using input () function.
+
+# a= input("Enter the value of a: ")
+
+# print("The type of a is ",type(a))
+
+
+#########4. Use comparison operator to find out whether a given variable is greater than 'b' or not . Take a=34 and b=80.
+
+# a=34
+# b=80
+# print(a>b)
+
+#########5. Write a python program to find an average of two numbers entered by the user.
+
+
+# a=int(input("Enter the value a: "))
+# b=int(input("Enter the value a: "))
+
+# average=(a+b)/2
+
+# print("The Average of a and b is: ",average)
+
+
+#########6. Write a python program to calculate the square of a number entered by the user.
+
+a=int(input("Enter the value: "))
+
+Square=a*a
+print("Square of entered value is: ",Square)
