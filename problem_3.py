773235698dcf9da891ee18353f6f197fe911d546
snapyy <210510113165@paruluniversity.ac.in>
2024-10-24 15:51:29 +0530

    Add files via upload

--- problem_3.py
+++ problem_3.py
@@ -10,14 +10,15 @@ Dear <|Name|>,
 You are selected!
 <|Date|>
 '''
-name=input("Enter your name sir! : ")
-Date=input("Enter the date on which you are available : ")
-print(letter.replace("<|Name|>",name),letter.replace("<|Date|>",Date))
+
+
+print(letter.replace("<|Name|>","Gajab").replace("<|Date|>","24-10-2024"))
 #########3. Write a program to detect double space in a string.
 j="hello ,  how are you , i am fine!"
 print(j.find("  "))
 #########4. Replace the double space from problem 3 with single spaces.
 
+print(j.replace("  "," "))
 #########5. Write a program to format the following letter using escape sequence characters.
 Letter="Dear Gaku,\n this is python course is nice .\n \t\"Thanks\"!"
 print(Letter)
